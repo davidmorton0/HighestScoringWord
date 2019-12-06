51f3cd26cf959a2fddf766bd33cd3f302fee9a3e
David Morton <55235981+davidmorton0@users.noreply.github.com>
2019-12-06 14:12:28 +0000

    add tests

--- highest_scoring_word.rb
+++ highest_scoring_word.rb
@@ -1,5 +1,13 @@
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
 def high(x)
   words = x.split
   scores = words.map { |word| word.chars.map { |ch| ch.ord - 96}.sum  }
   words[scores.index(scores.max)]
 end
+
+assert_equal(high('man i need a taxi up to ubud'), 'taxi')
+assert_equal(high('what time are we climbing up the volcano'), 'volcano')
+assert_equal(high('take me to semynak'), 'semynak')
+assert_equal(high('aaa b'), 'aaa')
